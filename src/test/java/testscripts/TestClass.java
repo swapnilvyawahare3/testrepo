48fc9b0588782de50558db5a75e1680054e384b1
Vyawahare <svyawaha@bmc.com>
2018-10-05 17:18:47 +0530

    Test Repo for Local Jenkins

---
+++ src/test/java/testscripts/TestClass.java
@@ -0,0 +1,11 @@
+package testscripts;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void  myMethod(){
+        System.out.println("This is test class");
+    }
+}
